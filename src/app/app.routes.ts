cfba862677f2046c241adfeb3f241b38554062eb
Guilherme Ferreira <guihensanfer@gmail.com>
2024-06-23 19:54:25 -0300

    commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,11 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { SsoComponent } from './sso/sso.component';
 
 export const routes: Routes = [
     {
-        path: 'sso/:lang',
-        children: [
-          { path: '', component: AppComponent },          
-          // Adicione outras rotas aqui
-        ]
-      },
-      { path: '**', redirectTo: '/sso/pt-br' } // Redirect to another route if the lang does not exists
+      path: 'sso/:lang',
+      component: SsoComponent
+    },
+    { path: '**', redirectTo: '/sso/pt-br' } // Redirect to another route if the lang does not exists
 ];
---
+++ src/app/sso/sso.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sso',
+  standalone: true,
+  imports: [],
+  templateUrl: './sso.component.html',
+  styleUrl: './sso.component.css'
+})
+export class SsoComponent {
+
+}
