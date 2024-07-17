926ddf0ee623a18de27de6ae757e7b8336c78f11
Guilherme Ferreira <guihensanfer@gmail.com>
2024-07-17 10:39:24 -0300

    routing redirect fix and not found component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,22 @@
 import { Routes } from '@angular/router';
 import { SsoComponent } from './sso/sso.component';
 import { environment } from '../environments/environment';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [    
     {
       path: 'sso/:lang/:secret',
       component: SsoComponent
-    },    
-    { path: '**', redirectTo: '/sso/' + environment.defaultLanguage } // Redirect to another route if the lang does not exists
+    },
+    {
+      path: 'sso/:secret',
+      redirectTo: (queryParams) => {
+        const secret = queryParams.params['secret'];
+        return `sso/${environment.defaultLanguage}/${secret}`;
+      }
+    },
+    { 
+      path: '**', 
+      component: NotFoundComponent
+    }
 ];
