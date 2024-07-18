9e5e0bc805b9fccff494e79a8ed8ffa84f678997
Guilherme Ferreira <guihensanfer@gmail.com>
2024-07-18 11:42:46 -0300

    error component ok

--- src/app/error/error.component.ts
+++ src/app/error/error.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { SeparatorElemComponent } from "../separator-elem/separator-elem.component";
+import { TranslocoModule } from '@ngneat/transloco';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-error',
   standalone: true,
-  imports: [],
+  imports: [SeparatorElemComponent, TranslocoModule],
   templateUrl: './error.component.html',
   styleUrl: './error.component.css'
 })
 export class ErrorComponent {
+  constructor(private router: Router) {}
 
+  refreshPage(): void {
+    window.location.reload();
+  }
 }
